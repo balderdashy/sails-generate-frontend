6e97b09c0635e0768aade50ce9abbb01b0b9a50d
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:56:01 -0700

    modify for gulp

--- templates/tasks/register/linkAssetsBuildProd.js
+++ templates/tasks/register/linkAssetsBuildProd.js
@@ -1,10 +1,11 @@
-module.exports = function (grunt) {
-	grunt.registerTask('linkAssetsBuildProd', [
-		'sails-linker:prodJsRelative',
-		'sails-linker:prodStylesRelative',
-		'sails-linker:devTpl',
-		'sails-linker:prodJsRelativeJade',
-		'sails-linker:prodStylesRelativeJade',
-		'sails-linker:devTplJade'
-	]);
+// todo - modify fully for gulp
+module.exports = function (gulp) {
+	//grunt.registerTask('linkAssetsBuildProd', [
+	//	'sails-linker:prodJsRelative',
+	//	'sails-linker:prodStylesRelative',
+	//	'sails-linker:devTpl',
+	//	'sails-linker:prodJsRelativeJade',
+	//	'sails-linker:prodStylesRelativeJade',
+	//	'sails-linker:devTplJade'
+	//]);
 };
