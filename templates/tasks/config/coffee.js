319ea53e01fdb01c59eb0221184ceddc84a9bd27
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:33:47 -0700

    updated for gulp

--- templates/tasks/config/coffee.js
+++ templates/tasks/config/coffee.js
@@ -9,30 +9,6 @@
  * For usage docs see:
  * 		https://github.com/gruntjs/grunt-contrib-coffee
  */
-module.exports = function(grunt) {
+module.exports = function(gulp, plugins) {
 
-	grunt.config.set('coffee', {
-		dev: {
-			options: {
-				bare: true,
-				sourceMap: true,
-				sourceRoot: './'
-			},
-			files: [{
-				expand: true,
-				cwd: 'assets/js/',
-				src: ['**/*.coffee'],
-				dest: '.tmp/public/js/',
-				ext: '.js'
-			}, {
-				expand: true,
-				cwd: 'assets/js/',
-				src: ['**/*.coffee'],
-				dest: '.tmp/public/js/',
-				ext: '.js'
-			}]
-		}
-	});
-
-	grunt.loadNpmTasks('grunt-contrib-coffee');
 };
