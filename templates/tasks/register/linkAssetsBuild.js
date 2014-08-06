c204c337adfb6e6d2a71219e29ad98071af55a2b
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:55:14 -0700

    modify for gulp

--- templates/tasks/register/linkAssetsBuild.js
+++ templates/tasks/register/linkAssetsBuild.js
@@ -1,10 +1,11 @@
-module.exports = function (grunt) {
-	grunt.registerTask('linkAssetsBuild', [
-		'sails-linker:devJsRelative',
-		'sails-linker:devStylesRelative',
-		'sails-linker:devTpl',
-		'sails-linker:devJsRelativeJade',
-		'sails-linker:devStylesRelativeJade',
-		'sails-linker:devTplJade'
-	]);
+// todo - modify fully for gulp
+module.exports = function (gulp) {
+	//grunt.registerTask('linkAssetsBuild', [
+	//	'sails-linker:devJsRelative',
+	//	'sails-linker:devStylesRelative',
+	//	'sails-linker:devTpl',
+	//	'sails-linker:devJsRelativeJade',
+	//	'sails-linker:devStylesRelativeJade',
+	//	'sails-linker:devTplJade'
+	//]);
 };
