29c0ebc2ff7b19b61f8afbb2002572ea70db6124
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:16:57 -0700

    changed to gulp

--- templates/tasks/config/watch.js
+++ templates/tasks/config/watch.js
@@ -12,23 +12,20 @@
  * 		https://github.com/gruntjs/grunt-contrib-watch
  *
  */
-module.exports = function(grunt) {
+module.exports = function(gulp, plugins) {
+	gulp.task('watch', function() {
+		// Watch .less files
+		gulp.watch('assets/styles/**/*.less', ['styles']);
+		// Watch .js files
+		gulp.watch('assets/js/**/*.js', ['scripts']);
+		// Watch image files
+		gulp.watch('assets/images/**/*', ['images']);
+		// Create LiveReload server
+		var server = plugins.livereload();
+		// Watch any files in dist/, reload on change
+		gulp.watch(['assets/**/**']).on('change', function(file) {
+			server.changed(file.path);
+		});
 
-	grunt.config.set('watch', {
-		api: {
-
-			// API files to watch:
-			files: ['api/**/*']
-		},
-		assets: {
-
-			// Assets to watch:
-			files: ['assets/**/*', 'tasks/pipeline.js'],
-
-			// When assets are changed:
-			tasks: ['syncAssets' <%- linker ? ", 'linkAssets'" : '' %>]
-		}
 	});
-
-	grunt.loadNpmTasks('grunt-contrib-watch');
 };
