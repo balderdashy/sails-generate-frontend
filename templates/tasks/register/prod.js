d65962252a7405a298e8631d0ad5110f1d3d3e73
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:56:48 -0700

    modify for gulp

--- templates/tasks/register/prod.js
+++ templates/tasks/register/prod.js
@@ -1,14 +1,15 @@
-module.exports = function (grunt) {
-	grunt.registerTask('prod', [
-		'compileAssets',
-		'concat',
-		'uglify',
-		'cssmin',
-		'sails-linker:prodJs',
-		'sails-linker:prodStyles',
-		'sails-linker:devTpl',
-		'sails-linker:prodJsJade',
-		'sails-linker:prodStylesJade',
-		'sails-linker:devTplJade'
-	]);
+// todo - modify fully for gulp
+module.exports = function (gulp) {
+	//grunt.registerTask('prod', [
+	//	'compileAssets',
+	//	'concat',
+	//	'uglify',
+	//	'cssmin',
+	//	'sails-linker:prodJs',
+	//	'sails-linker:prodStyles',
+	//	'sails-linker:devTpl',
+	//	'sails-linker:prodJsJade',
+	//	'sails-linker:prodStylesJade',
+	//	'sails-linker:devTplJade'
+	//]);
 };
