6c3957dc497c5d51b9e7274cbc259af7eab8fc3f
Matthew Marino <mmarino@jcmar.com>
2014-08-06 09:41:15 -0700

    updated for gulp

--- templates/tasks/config/jst.js
+++ templates/tasks/config/jst.js
@@ -12,34 +12,15 @@
  *
  */
 
-module.exports = function(grunt) {
+module.exports = function(gulp, plugins) {
 
 	var templateFilesToInject = [
 		'templates/**/*.html'
 	];
 
-	grunt.config.set('jst', {
-		dev: {
-
-			// To use other sorts of templates, specify a regexp like the example below:
-			// options: {
-			//   templateSettings: {
-			//     interpolate: /\{\{(.+?)\}\}/g
-			//   }
-			// },
-
-			// Note that the interpolate setting above is simply an example of overwriting lodash's
-			// default interpolation. If you want to parse templates with the default _.template behavior
-			// (i.e. using <div><%= this.id %></div>), there's no need to overwrite `templateSettings.interpolate`.
-
-
-			files: {
-				// e.g.
-				// 'relative/path/from/gruntfile/to/compiled/template/destination'  : ['relative/path/to/sourcefiles/**/*.html']
-				'.tmp/public/jst.js': require('../pipeline').templateFilesToInject
-			}
-		}
+	gulp.task('jst', function() {
+		gulp.src(require('../pipeline').templateFilesToInject)
+		.pipe(plugins.jst())
+		.pipe(gulp.dest('.tmp/public/jst.js'));
 	});
-
-	grunt.loadNpmTasks('grunt-contrib-jst');
 };
