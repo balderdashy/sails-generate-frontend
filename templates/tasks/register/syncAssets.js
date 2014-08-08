97a8b34af1521b022cdeb7036d4705bce715eb5f
Matthew Marino <mmarino@jcmar.com>
2014-08-07 23:39:38 -0700

    Update syncAssets.js

--- templates/tasks/register/syncAssets.js
+++ templates/tasks/register/syncAssets.js
@@ -1,9 +1,9 @@
 // todo - modify fully for gulp
 module.exports = function (gulp) {
-	//grunt.registerTask('syncAssets', [
-	//	'jst:dev',
-	//	'less:dev',
-	//	'sync:dev',
-	//	'coffee:dev'
-	//]);
+	gulp.task('syncAssets', [
+		'jst:dev',
+		'less:dev',
+		'sync:dev',
+		'coffee:dev'
+	]);
 };
