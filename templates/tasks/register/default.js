6e8cecc2996818f993af97a6318d480430cf5468
Matthew Marino <mmarino@jcmar.com>
2014-08-07 23:36:16 -0700

    Update default.js

--- templates/tasks/register/default.js
+++ templates/tasks/register/default.js
@@ -1,5 +1,4 @@
-// todo - modify fully for gulp
 module.exports = function (gulp) {
-	//grunt.registerTask('default', ['compileAssets', <%- linker ? "'linkAssets', " : '' %> 'watch']);
-	gulp.task('default', ['styles', 'copy', 'scripts', 'images', 'linker', 'watch']);
+	//grunt.registerTask('default', ['compileAssets', 'linkAssets',  'watch']);
+	gulp.task('default', ['compileAssets', 'images', 'linkAssets', 'watch:api', 'watch:assets']);
 };
