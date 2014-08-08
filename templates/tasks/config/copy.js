ec59a80b0c5e9700a786cea27ef8a8d6db492731
Matthew Marino <mmarino@jcmar.com>
2014-08-07 23:27:19 -0700

    Update copy.js

--- templates/tasks/config/copy.js
+++ templates/tasks/config/copy.js
@@ -14,9 +14,14 @@
  * 		https://github.com/gruntjs/grunt-contrib-copy
  */
 module.exports = function(gulp, plugins) {
-	gulp.task('copy', function() {
-		return gulp.src('./assets/**/**.!(coffee|less|css|js)')
+	gulp.task('copy:dev', ['clean:dev', 'jst:dev'], function() {
+		return gulp.src('./assets/**/*.!(coffee|less)')
 				.pipe(gulp.dest('.tmp/public'))
-				.pipe(plugins.notify({ message: 'Copy task complete' }));
+				.pipe(plugins.notify({ message: 'Copy dev task complete' }));
+	});
+	gulp.task('copy:build', ['clean:dev', 'jst:dev'], function() {
+		return gulp.src('.tmp/public/**/*')
+				.pipe(gulp.dest('www'))
+				.pipe(plugins.notify({ message: 'Copy build task complete' }));
 	});
 };
