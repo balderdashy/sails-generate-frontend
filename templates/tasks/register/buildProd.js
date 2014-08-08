8dc649aa91b54036e23cc27afcba4a82beebaf85
Matthew Marino <mmarino@jcmar.com>
2014-08-07 23:35:28 -0700

    Update buildProd.js

--- templates/tasks/register/buildProd.js
+++ templates/tasks/register/buildProd.js
@@ -1,12 +1,11 @@
-// todo - modify fully for gulp
 module.exports = function (gulp) {
-	//grunt.registerTask('buildProd', [
-	//	'compileAssets',
-	//	'concat',
-	//	'uglify',
-	//	'cssmin',
-	//	'linkAssetsBuildProd',
-	//	'clean:build',
-	//	'copy:build'
-	//]);
+	gulp.task('buildProd', [
+		'compileAssets',
+		'concat',
+		'uglify',
+		'cssmin',
+		'linkAssetsBuildProd',
+		'clean:build',
+		'copy:build'
+	]);
 };
