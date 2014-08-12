48ef0895c265af4e9fba08c90fe8511cd301d7c5
Matthew Marino <mmarino@jcmar.com>
2014-08-12 12:59:50 -0700

    modified to use with run-sequence

--- templates/tasks/register/linkAssetsBuildProd.js
+++ templates/tasks/register/linkAssetsBuildProd.js
@@ -1,11 +1,13 @@
-// todo - modify fully for gulp
-module.exports = function (gulp) {
-	gulp.task('linkAssetsBuildProd', [
-		'sails-linker-gulp:prodJsRelative',
-		'sails-linker-gulp:prodStylesRelative',
-		'sails-linker-gulp:devTpl'/* ,
-		'sails-linker-gulp:prodJsRelativeJade',
-		'sails-linker-gulp:prodStylesRelativeJade',
-		'sails-linker-gulp:devTplJade' */
-	]);
+module.exports = function (gulp, plugins) {
+	gulp.task('linkAssetsBuildProd', function(cb) {
+		plugins.sequence(
+			'sails-linker-gulp:prodJsRelative',
+			'sails-linker-gulp:prodStylesRelative',
+			'sails-linker-gulp:devTpl',
+			/*'sails-linker-gulp:prodJsRelativeJade',
+			'sails-linker-gulp:prodStylesRelativeJade',
+			'sails-linker-gulp:devTplJade' */
+			cb
+		);
+	});
 };
