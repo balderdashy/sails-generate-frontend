9ea58390e62bebfcda190036ab0730f6310c6d61
Matthew Marino <mmarino@jcmar.com>
2014-08-12 12:45:18 -0700

    updated for gulp

--- templates/tasks/config/cssmin.js
+++ templates/tasks/config/cssmin.js
@@ -5,8 +5,6 @@
  *
  * Minifies css files and places them into .tmp/public/min directory.
  *
- * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-cssmin
  */
 module.exports = function(gulp, plugins) {
 
