c77554b1e46d05ef85c09f0d47516a652123f0d2
Matthew Marino <mmarino@jcmar.com>
2014-08-12 12:41:06 -0700

    updated for gulp

--- templates/tasks/config/clean.js
+++ templates/tasks/config/clean.js
@@ -3,21 +3,19 @@
  *
  * ---------------------------------------------------------------
  *
- * This grunt task is configured to clean out the contents in the .tmp/public of your
+ * This gulp task is configured to clean out the contents in the .tmp/public of your
  * sails project.
  *
- * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-clean
  */
 module.exports = function(gulp, plugins) {
 	gulp.task('clean:dev', function() {
 		return gulp.src('.tmp/public/**', {read: false})
 				.pipe(plugins.rimraf({ force: true }))
-				.pipe(plugins.notify({ message: 'Clean task complete' }));;
+				.pipe(plugins.notify({ message: 'Clean task complete' }));
 	});
 	gulp.task('clean:build', function() {
 		return gulp.src('www', {read: false})
 				.pipe(plugins.rimraf({ force: true }))
-				.pipe(plugins.notify({ message: 'Clean task complete' }));;
+				.pipe(plugins.notify({ message: 'Clean task complete' }));
 	});
 };
