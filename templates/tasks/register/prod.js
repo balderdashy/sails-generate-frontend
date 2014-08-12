b82e7df27949795e4c5e7dc918657160f81cb9ba
Matthew Marino <mmarino@jcmar.com>
2014-08-12 13:00:22 -0700

    modified to use with run-sequence

--- templates/tasks/register/prod.js
+++ templates/tasks/register/prod.js
@@ -1,15 +1,17 @@
-// todo - modify fully for gulp
-module.exports = function (gulp) {
-	gulp.task('prod', [
-		'compileAssets',
-		'concat',
-		'uglify',
-		'cssmin',
-		'sails-linker-gulp:prodJs',
-		'sails-linker-gulp:prodStyles',
-		'sails-linker-gulp:devTpl'/* ,
-		'sails-linker-gulp:prodJsJade',
-		'sails-linker-gulp:prodStylesJade',
-		'sails-linker-gulp:devTplJade' */
-	]);
+module.exports = function (gulp, plugins) {
+	gulp.task('prod', function(cb) {
+		plugins.sequence(
+			'compileAssets',
+			'concat',
+			'uglify',
+			'cssmin',
+			'sails-linker-gulp:prodJs',
+			'sails-linker-gulp:prodStyles',
+			'sails-linker-gulp:devTpl',
+			/*'sails-linker-gulp:prodJsJade',
+			'sails-linker-gulp:prodStylesJade',
+			'sails-linker-gulp:devTplJade' */
+			cb
+		);
+	});
 };
