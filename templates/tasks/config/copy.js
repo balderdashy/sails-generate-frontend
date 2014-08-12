b83ad1e4efccba533e1f5da24a7bd00e1b0477eb
Matthew Marino <mmarino@jcmar.com>
2014-08-12 12:44:41 -0700

    removed images folder from being copied

--- templates/tasks/config/copy.js
+++ templates/tasks/config/copy.js
@@ -10,16 +10,14 @@
  * # build task config
  * Copies all directories nd files from the .tmp/public directory into a www directory.
  *
- * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-copy
  */
 module.exports = function(gulp, plugins) {
-	gulp.task('copy:dev', ['clean:dev', 'jst:dev'], function() {
-		return gulp.src('./assets/**/*.!(coffee|less)')
+	gulp.task('copy:dev', function() {
+		return gulp.src(['./assets/**/*.!(coffee|less)', '!assets/images{,/**}'])
 				.pipe(gulp.dest('.tmp/public'))
 				.pipe(plugins.notify({ message: 'Copy dev task complete' }));
 	});
-	gulp.task('copy:build', ['clean:dev', 'jst:dev'], function() {
+	gulp.task('copy:build', function() {
 		return gulp.src('.tmp/public/**/*')
 				.pipe(gulp.dest('www'))
 				.pipe(plugins.notify({ message: 'Copy build task complete' }));
