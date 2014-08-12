a698947494fa9ca39c322cdcae91460d757a74b4
Matthew Marino <mmarino@jcmar.com>
2014-08-12 12:42:19 -0700

    updated for gulp

--- templates/tasks/config/coffee.js
+++ templates/tasks/config/coffee.js
@@ -6,8 +6,6 @@
  * Compiles coffeeScript files from `assest/js` into Javascript and places them into
  * `.tmp/public/js` directory.
  *
- * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-coffee
  */
 module.exports = function(gulp, plugins) {
 	gulp.task('coffee:dev', function() {
