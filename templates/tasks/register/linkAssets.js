de6f592b147b81a27f2e53638724e5fb27833759
Matthew Marino <mmarino@jcmar.com>
2014-08-12 12:58:32 -0700

    modified to use with run-sequence

--- templates/tasks/register/linkAssets.js
+++ templates/tasks/register/linkAssets.js
@@ -1,11 +1,13 @@
-// todo - modify fully for gulp
-module.exports = function (gulp) {
-	gulp.task('linkAssets', [
-		'sails-linker-gulp:devJs',
-		'sails-linker-gulp:devStyles',
-		'sails-linker-gulp:devTpl'/* ,
-		'sails-linker-gulp:devJsJade',
-		'sails-linker-gulp:devStylesJade',
-		'sails-linker-gulp:devTplJade' */
-	]);
+module.exports = function (gulp, plugins) {
+	gulp.task('linkAssets', function(cb) {
+		plugins.sequence(
+			'sails-linker-gulp:devJs',
+			'sails-linker-gulp:devStyles',
+			'sails-linker-gulp:devTpl',
+			/*'sails-linker-gulp:devJsJade',
+			'sails-linker-gulp:devStylesJade',
+			'sails-linker-gulp:devTplJade' */
+			cb
+		);
+	});
 };
