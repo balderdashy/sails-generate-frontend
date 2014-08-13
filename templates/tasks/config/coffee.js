bd8008d2fc09c57a259d737f59d8bf88919df064
Matthew Marino <mmarino@jcmar.com>
2014-08-13 11:13:30 -0700

    added toggle for gulp-notify

--- templates/tasks/config/coffee.js
+++ templates/tasks/config/coffee.js
@@ -7,10 +7,11 @@
  * `.tmp/public/js` directory.
  *
  */
-module.exports = function(gulp, plugins) {
+module.exports = function(gulp, plugins, growl) {
 	gulp.task('coffee:dev', function() {
 		gulp.src('assest/js/**/**.coffee')
 		.pipe(plugins.coffee({bare: true}).on('error', plugins.util.log))
 		.pipe(gulp.dest('.tmp/public/js/'))
+		.pipe(plugins.if(growl, plugins.notify({ message: 'Coffee compile task complete' })));
 	});
 };
