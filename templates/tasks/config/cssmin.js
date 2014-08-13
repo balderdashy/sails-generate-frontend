54e92d54a262b4221c35c68099500cd8c1279d13
Matthew Marino <mmarino@jcmar.com>
2014-08-13 11:15:05 -0700

    added toggle for gulp-notify

--- templates/tasks/config/cssmin.js
+++ templates/tasks/config/cssmin.js
@@ -6,13 +6,13 @@
  * Minifies css files and places them into .tmp/public/min directory.
  *
  */
-module.exports = function(gulp, plugins) {
+module.exports = function(gulp, plugins, growl) {
 
 	gulp.task('cssmin:dist', function() {
 		return gulp.src('.tmp/public/concat/production.css')
 				.pipe(plugins.rename({ suffix: '.min' }))
 				.pipe(plugins.minifyCss())
 				.pipe(gulp.dest('./.tmp/public/min'))
-				.pipe(plugins.notify({ message: 'Minify CSS task complete' }));
+				.pipe(plugins.if(growl, plugins.notify({ message: 'Minify CSS task complete' })));
 		});
 };
