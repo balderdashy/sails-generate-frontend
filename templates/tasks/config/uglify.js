79eae74befdabf79ec841fe104846fce5e8d26ef
Matthew Marino <mmarino@jcmar.com>
2014-08-13 11:18:29 -0700

    added toggle for gulp-notify

--- templates/tasks/config/uglify.js
+++ templates/tasks/config/uglify.js
@@ -6,13 +6,13 @@
  * Minifies client-side javascript `assets`.
  *
  */
-module.exports = function(gulp, plugins) {
+module.exports = function(gulp, plugins, growl) {
 
 	gulp.task('uglify:dist', function() {
 		return gulp.src('.tmp/public/concat/production.js')
 				.pipe(plugins.rename({ suffix: '.min' }))
 				.pipe(plugins.uglify(/* {mangle: true} */))
 				.pipe(gulp.dest('.tmp/public/min'))
-				.pipe(plugins.notify({ message: 'uglify dist task complete' }));
+				.pipe(plugins.if(growl, plugins.notify({ message: 'uglify dist task complete' })));
 		});
 };
