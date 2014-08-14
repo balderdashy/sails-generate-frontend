296042c08ac939b697fd2a05a824a9bb85ea814a
Matthew Marino <mmarino@jcmar.com>
2014-08-14 12:12:57 -0700

    fixed clean files and folders

--- templates/tasks/config/clean.js
+++ templates/tasks/config/clean.js
@@ -9,12 +9,12 @@
  */
 module.exports = function(gulp, plugins, growl) {
 	gulp.task('clean:dev', function() {
-		return gulp.src('.tmp/public/**', {read: false})
+		return gulp.src(['.tmp/public/**/*.*', '.tmp/public{,/**}'], {read: false})
 				.pipe(plugins.rimraf({ force: true }))
 				.pipe(plugins.if(growl, plugins.notify({ message: 'Clean task complete' })));
 	});
 	gulp.task('clean:build', function() {
-		return gulp.src('www', {read: false})
+		return gulp.src(['www/**/*.*', 'www{,/**}'], {read: false})
 				.pipe(plugins.rimraf({ force: true }))
 				.pipe(plugins.if(growl, plugins.notify({ message: 'Clean task complete' })));
 	});
