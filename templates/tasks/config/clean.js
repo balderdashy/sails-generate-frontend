2eedeb7418aa9ded2af664b3ec26d62cb9ee785d
Matthew Marino <mmarino@jcmar.com>
2014-10-09 11:00:55 -0700

    clean task fix
    gulp-rimraf has issues with files/folder names that have more than 1 dot
    in them

--- templates/tasks/config/clean.js
+++ templates/tasks/config/clean.js
@@ -7,11 +7,15 @@
  * sails project.
  *
  */
+ var rimraf = require('rimraf');
 module.exports = function(gulp, plugins, growl) {
-	gulp.task('clean:dev', function() {
-		return gulp.src(['.tmp/public/**/*.*', '.tmp/public{,/**}'], {read: false})
-				.pipe(plugins.rimraf({ force: true }))
-				.pipe(plugins.if(growl, plugins.notify({ message: 'Clean task complete' })));
+	// gulp.task('clean:dev', function() {
+		// return gulp.src(['.tmp/public/**/*.*', '.tmp/public{,/**}'], {read: false})
+				// .pipe(plugins.rimraf({ force: true }))
+				// .pipe(plugins.if(growl, plugins.notify({ message: 'Clean task complete' })));
+	// });
+	gulp.task('clean:dev', function(cb) {
+      return rimraf('.tmp/public', cb);
 	});
 	gulp.task('clean:build', function() {
 		return gulp.src(['www/**/*.*', 'www{,/**}'], {read: false})
