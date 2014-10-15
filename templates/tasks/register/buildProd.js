21d4fbadd8783eea160eb525b9cd54202262a055
Andreas Heintze <andreas@heintze.se>
2014-10-15 07:52:48 +0200

    Fixed Gulp tasks - Linking

--- templates/tasks/register/buildProd.js
+++ templates/tasks/register/buildProd.js
@@ -2,9 +2,10 @@ module.exports = function (gulp, plugins) {
 	gulp.task('buildProd', function(cb) {
 		plugins.sequence(
 			'compileAssets',
-			'concat',
-			'uglify',
-			'cssmin',
+			'concat:js',
+			'concat:css',
+			'uglify:dist',
+			'cssmin:dist',
 			'linkAssetsBuildProd',
 			'clean:build',
 			'copy:build',
--- templates/tasks/register/linkAssetsBuild.js
+++ templates/tasks/register/linkAssetsBuild.js
@@ -1,12 +1,8 @@
 module.exports = function (gulp, plugins) {
 	gulp.task('linkAssetsBuild', function(cb) {
 		plugins.sequence(
-			'sails-linker-gulp:devJsRelative',
-			'sails-linker-gulp:devStylesRelative',
-			'sails-linker-gulp:devTpl',
-			/*'sails-linker-gulp:devJsRelativeJade',
-			'sails-linker-gulp:devStylesRelativeJade',
-			'sails-linker-gulp:devTplJade' */
+			'sails-linker-gulp:devAssetsRelative',
+			'sails-linker-gulp:devViewsRelative',
 			cb
 		);
 	});
