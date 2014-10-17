8eff9e496e6749cf654b7198ccd8794fec849d69
Matthew Marino <mmarino@jcmar.com>
2014-10-16 21:03:25 -0700

    removed cache from images task, it was causing problems.

--- templates/tasks/config/images.js
+++ templates/tasks/config/images.js
@@ -8,7 +8,7 @@
 module.exports = function(gulp, plugins, growl) {
 	 gulp.task('images', function() {
 		return gulp.src('assets/images/**/*')
-				.pipe(plugins.cache(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true })))
+				.pipe(plugins.imagemin({ optimizationLevel: 3, progressive: true, interlaced: true }))
 				.pipe(gulp.dest('.tmp/public/images'))
 				.pipe(plugins.if(growl, plugins.notify({ message: 'Images task complete' })));
 	});
