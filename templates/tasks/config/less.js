9fef2cf2518e7fffdcee3204d78b946cd7cabb04
Marco Pegoraro <marco.pegoraro@gmail.com>
2015-12-29 12:21:38 +0100

    added include paths to less grunt task
    it allows to `@import` from bower and node modules:
    
    ```
    # console
    npm install bootstrap
    
    # assets/styles/importer.less
    @import "bootstrap/less/bootstrap.less"
    ``

--- templates/tasks/config/less.js
+++ templates/tasks/config/less.js
@@ -8,21 +8,24 @@
  * dependencies, mixins, variables, resets, etc. before other stylesheets)
  *
  * For usage docs see:
- * 		https://github.com/gruntjs/grunt-contrib-less
+ *      https://github.com/gruntjs/grunt-contrib-less
  */
 module.exports = function(grunt) {
 
-	grunt.config.set('less', {
-		dev: {
-			files: [{
-				expand: true,
-				cwd: 'assets/styles/',
-				src: ['importer.less'],
-				dest: '.tmp/public/styles/',
-				ext: '.css'
-			}]
-		}
-	});
+    grunt.config.set('less', {
+        dev: {
+            files: [{
+                expand: true,
+                cwd: 'assets/styles/',
+                src: ['importer.less'],
+                dest: '.tmp/public/styles/',
+                ext: '.css'
+            }],
+            options: {
+                paths: ['bower_components', 'node_modules']
+            }
+        }
+    });
 
-	grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-less');
 };
