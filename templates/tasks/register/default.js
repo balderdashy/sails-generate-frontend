ffd998ee0e5eeecdd29c5ac66bd13ce6375c41e7
Mike McNeil <mikermcneil@users.noreply.github.com>
2016-03-10 16:23:18 -0600

    Update default.js

--- templates/tasks/register/default.js
+++ templates/tasks/register/default.js
@@ -8,9 +8,14 @@
  * called automatically when you start Sails in development mode using
  * `sails lift` or `node app`.
  *
- * Note that when lifting your app in a production environment (with the
- * `NODE_ENV` environment variable set to 'production') the `prod` task
- * will be run instead of this one.
+ * Note that when lifting your app with a custom environment setting
+ * (i.e. `sails.config.environment`), Sails will look for a tasklist file
+ * with the same name and run that instead of this one.
+ * 
+ * > Note that as a special case for compatibility/historial reasons, if
+ * > your environment is "production", and Sails cannot find a tasklist named
+ * > `production.js`, it will attempt to run the `prod.js` tasklist as well
+ * > before defaulting to `default.js`.
  *
  * For more information see:
  *   http://sailsjs.org/documentation/anatomy/my-app/tasks/register/default-js
