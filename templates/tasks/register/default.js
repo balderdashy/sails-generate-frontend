359aaa9e35fe22331b2c8abf3c3ec864d52aabb5
Mike McNeil <mike@balderdash.co>
2016-10-08 23:38:17 -0500

    Remove need for unescaping in tasks/register/default.js template.

--- templates/tasks/register/default.js
+++ templates/tasks/register/default.js
@@ -11,7 +11,7 @@
  * Note that when lifting your app with a custom environment setting
  * (i.e. `sails.config.environment`), Sails will look for a tasklist file
  * with the same name and run that instead of this one.
- * 
+ *
  * > Note that as a special case for compatibility/historial reasons, if
  * > your environment is "production", and Sails cannot find a tasklist named
  * > `production.js`, it will attempt to run the `prod.js` tasklist as well
@@ -22,5 +22,29 @@
  *
  */
 module.exports = function (grunt) {
-  grunt.registerTask('default', ['compileAssets', <%- linker ? "'linkAssets', " : '' %> 'watch']);
+
+  <%
+  //  ┌─┐┌─┐┬┬  ┌─┐   ┬  ┬┌┐┌┬┌─┌─┐┬─┐  ╔═╗╔╗╔╔═╗╔╗ ╦  ╔═╗╔╦╗
+  //  └─┐├─┤││  └─┐───│  ││││├┴┐├┤ ├┬┘  ║╣ ║║║╠═╣╠╩╗║  ║╣  ║║
+  //  └─┘┴ ┴┴┴─┘└─┘   ┴─┘┴┘└┘┴ ┴└─┘┴└─  ╚═╝╝╚╝╩ ╩╚═╝╩═╝╚═╝═╩╝ooo
+  //  ┌─    ┌┬┐┌─┐┌─┐┌─┐┬ ┬┬ ┌┬┐    ─┐
+  //  │───   ││├┤ ├┤ ├─┤│ ││  │   ───│
+  //  └─    ─┴┘└─┘└  ┴ ┴└─┘┴─┘┴     ─┘
+  if (linker) { %>
+    grunt.registerTask('default', [
+      'compileAssets',
+      'linkAssets',
+      'watch'
+    ]);
+  <% }
+  //  ┌─┐┌─┐┬┬  ┌─┐   ┬  ┬┌┐┌┬┌─┌─┐┬─┐  ╔╦╗╦╔═╗╔═╗╔╗ ╦  ╔═╗╔╦╗
+  //  └─┐├─┤││  └─┐───│  ││││├┴┐├┤ ├┬┘   ║║║╚═╗╠═╣╠╩╗║  ║╣  ║║
+  //  └─┘┴ ┴┴┴─┘└─┘   ┴─┘┴┘└┘┴ ┴└─┘┴└─  ═╩╝╩╚═╝╩ ╩╚═╝╩═╝╚═╝═╩╝ooo
+  else { %>
+    grunt.registerTask('default', [
+      'compileAssets',
+      'watch'
+    ]);
+  <% } %>
+
 };
